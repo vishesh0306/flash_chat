e56e70403ebcfdec90086a672743700167d2eda9
vishesh0306 <115298750+vishesh0306@users.noreply.github.com>
2023-08-15 21:26:59 +0530

    Animation work completed

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash/screens/login_screen.dart';
 import 'package:flash/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -8,76 +9,103 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+
+  AnimationController? controller;
+  Animation? animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(duration: Duration(seconds: 1) , vsync: this);
+    animation = ColorTween(begin: Colors.white,end: Colors.blueGrey).animate(controller as Animation<double>);
+
+  controller?.forward();
+  controller?.addListener(() {
+    setState(() {});
+  });
+  }
+
+  @override
+  void dispose(){
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    child: Image.asset('images/logo.png'),
-                    height: 60.0,
-                  ),
+      backgroundColor: animation?.value,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  child: Image.asset('images/logo.png'),
+                  height: (controller!.value*90),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
+              ),
+
+              AnimatedTextKit(
+                animatedTexts: [TypewriterAnimatedText('Flash Chat',
+              textStyle: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.w900,
+
+              ),speed: Duration(milliseconds: 400)
+              )
+
               ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 28.0,
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0,horizontal: 24),
+            child: Material(
+              elevation: 5.0,
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(30.0),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, LoginScreen.id);
+                },
+                minWidth: 200.0,
+                height: 42.0,
+                child: Text(
+                  'Log In',
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0,horizontal: 24),
+            child: Material(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(30.0),
+              elevation: 5.0,
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, RegistrationScreen.id);
+                },
+                minWidth: 200.0,
+                height: 42.0,
+                child: Text(
+                  'Register',
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
