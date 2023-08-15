b39f5a2576d4125cb7c6949fa5cd84b87f67ccea
vishesh0306 <115298750+vishesh0306@users.noreply.github.com>
2023-08-16 00:52:25 +0530

    Code is Refactored!

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flash/components/rounded_button.dart';
 import 'package:flash/screens/login_screen.dart';
 import 'package:flash/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -68,43 +69,23 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             height: 28.0,
           ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0,horizontal: 24),
-            child: Material(
-              elevation: 5.0,
-              color: Colors.lightBlueAccent,
-              borderRadius: BorderRadius.circular(30.0),
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
-                minWidth: 200.0,
-                height: 42.0,
-                child: Text(
-                  'Log In',
-                ),
-              ),
-            ),
+
+          RoundedButton(
+              Colors.lightBlueAccent,
+              'Log In',
+                  (){Navigator.pushNamed(context, LoginScreen.id);},
+
+
           ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0,horizontal: 24),
-            child: Material(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.circular(30.0),
-              elevation: 5.0,
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RegistrationScreen.id);
-                },
-                minWidth: 200.0,
-                height: 42.0,
-                child: Text(
-                  'Register',
-                ),
-              ),
-            ),
+
+          RoundedButton(
+              Colors.blueAccent,
+              'Register',
+                  (){Navigator.pushNamed(context, RegistrationScreen.id);}
+
           ),
+
         ],
       ),
     );
